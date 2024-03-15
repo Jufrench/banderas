7b9c90d87ceda3c98bb258a0c49f1562459ba6ad
Jufrench <ju.french@gmail.com>
2024-03-15 00:38:51 -0500

    Initial setup, modal, flags in background, got api data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,18 @@
 'use client'
 
+import { useDisclosure } from '@mantine/hooks';
+import { Modal, Card, Paper, Title, Image, Button } from '@mantine/core';
 import { useState, useEffect } from 'react'
+
 import styles from "./page.module.css";
 
 export default function Home() {
 
   const [countries, setCountries] = useState<{}[]>([]);
-  // const [countries, setCountries] = useState<{}[] | null>(null);
 
   useEffect(() => {
-    // if (countries.length !== 0) {
-    //   return;
-    // } else {
-    //   fetch("https://restcountries.com/v3.1/all")
-    //   .then(res => res.json())
-    //   .then(res => setCountries(res))
-    //   .catch(err => console.log(err)) 
-    // }
-// ================================
-    // if (countries.length !== 0) return;
-    // fetch("https://restcountries.com/v3.1/all")
-    //   .then(res => res.json())
-    //   .then(res => setCountries(res))
-    //   .catch(err => console.log(err)) 
-// ================================
-    // if (countries.length !== 0) return;
-    // fetchData();
-// ================================
-    // if (countries.length < 1) {
-    //   fetchData();
-    // }
-// ================================
-    // if (countries.length > 0) {
-    //   return;
-    // } else {
-    //   fetchData();
-    // }
-// ================================
-    // if (countries.length === 0) fetchData();
-// ================================ 
     if (countries.length === 0) {
+      console.log('fetching countries')
       fetch("https://restcountries.com/v3.1/all")
         .then(res => res.json())
         .then(res => setCountries(res))
@@ -47,36 +20,41 @@ export default function Home() {
     }
   }, [countries.length])
 
-
-
   console.log('%ccountries:', 'color:tomato', countries);
 
-  // const fetchData = () => {
-  //   fetch("https://restcountries.com/v3.1/all")
-  //   .then(res => res.json())
-  //   .then(res => setCountries(res))
-  //   .catch(err => console.log(err))
-  // }
-
-  // const getData = async () => {
-  //   try {
-  //     const res = fetch("https://restcountries.com/v3.1/all")
-  //     const data = (await res).json();
-      
-  //     console.log('%cdata:', 'color:tomato', data);
-  //   } catch(err) {
-  //     console.log('%cerror:', 'color:tomato', err)
-  //   }
-  // }
+  // window.scrollTo(0, document.body.scrollHeight);
+  // window.scroll({top: document.body.scrollHeight, behavior: "smooth"});
+  const [opened, { open, close }] = useDisclosure(true);
 
   return (
-    <main className={styles.main}>
-      {countries.length > 0 &&
-        countries.map((country: { [key: string]: any}) => {
-          console.log('%ccountry:', 'color:tomato', country);
-          return <div>{country.name.common}</div>
-        })
-      }
-    </main>
+    <>
+      <Modal className='julian' opened={opened} onClose={close} title="Welcome to Banderas!" centered>
+        <Button>Quick Play</Button>
+      </Modal>
+      <main className={styles.main}>
+        <ul
+          style={{
+            margin: 0, listStyleType: 'none', display: 'flex', flexWrap: 'wrap', gap: 40,
+            // opacity: 0.7,
+            backgroundColor: 'rgba(0, 0, 0, 0.1)',
+          }}>
+          {countries.length > 0 &&
+            countries.map((country: { [key: string]: any}, index) => {
+              return (
+                <li key={index}>
+                  {/* <Paper
+                    style={{
+                      padding: '1.25rem'
+                    }}>
+                    <Title order={5}>{country.name.common}</Title>
+                  </Paper> */}
+                  <Image w={100} h={50} src={country.flags.png} />
+                </li>
+              )
+            })
+          }
+        </ul>
+      </main>
+    </>
   );
 }
