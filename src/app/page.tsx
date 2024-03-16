98646802b6f2c240d3457b5baa8fe077f3e96b83
Jufrench <ju.french@gmail.com>
2024-03-15 19:32:15 -0500

    Show quick play model Can skip country - need to fix countdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,176 @@
 'use client'
 
 import { useDisclosure } from '@mantine/hooks';
-import { Modal, Card, Paper, Title, Image, Button } from '@mantine/core';
-import { useState, useEffect } from 'react'
+import { Modal, Stack, Box, Tabs, TextInput, Card, Paper, Title, Image, Button } from '@mantine/core';
+import { useState, useEffect, use } from 'react'
 
 import styles from "./page.module.css";
 
-export default function Home() {
+interface QuickPlayModalProps {
+  countries: {}[];
+}
+
+interface SkipButtonProps {
+  handleSkipCountry: () => void;
+  skipCountry: boolean;
+}
+const SkipButton = (props: SkipButtonProps) => {
+  // const [skipCountry, setSkipCountry] = useState<boolean>(false);
+  const [countdown, setCountdown] = useState<number>(3);
+
+  useEffect(() => {
+    if (props.skipCountry) {
+      const interval = setInterval(() => {
+        setCountdown(countdown - 1);
+      }, 1000);
+      if (countdown === 0) {
+        // setSkipCountry(false);
+        // props.onClick();
+        setCountdown(3);
+        clearInterval(interval);
+      }
+      return () => clearInterval(interval);
+    }
+  }, [props.skipCountry, countdown])
+
+  return (
+    <Button onClick={props.handleSkipCountry} w="fit-content" color="tomato" size="compact-sm">
+      {props.skipCountry ? `Skipping in ${countdown}`: 'Skip'}
+    </Button>
+  )
+}
+
+// interface Country {
+//   name: {
+//     common: string;
+//   };
+//   flag: string;
+//   flags: {
+//     png: string;
+//     svg: string;
+//   }
+// }
+type Country = {
+  name: {
+    common: string;
+  },
+  flag: string,
+  flags: {
+    png: string;
+    svg: string;
+  }
+}
+
+function QuickPlayModal(props: QuickPlayModalProps): JSX.Element {
+  const [opened, { open, close }] = useDisclosure(true);
+
+  // const randonCountry = (props.countries[Math.floor(Math.random() * 250)] as Country);
+  // const [activeCountry, setActiveCountry] = useState<string | null>(randonCountry.name.common);
+  const [activeCountry, setActiveCountry] = useState<{} | null>((props.countries[Math.floor(Math.random() * 250)] as Country));
+  const [skipCountry, setSkipCountry] = useState<boolean>(false);
+
+  console.group('%c    ', 'background: white')
+  // console.log('props.countries:', (props.countries[Math.floor(Math.random() * 250)] as Country).name.common);
+  // console.log('randonCountry:', randonCountry.name.common);
+  console.log('activeCountry:', activeCountry);
+  console.groupEnd()
+
+  useEffect(() => {
+    if (skipCountry) {
+      const newRandonCountry = (props.countries[Math.floor(Math.random() * 250)] as Country);
+      setActiveCountry(newRandonCountry);
+      setSkipCountry(false);
+    }
+  }, [skipCountry])
+
+  const handleSkipCountry = () => {
+    console.log('%chandleSkipCountry', 'color:gold')
+    setSkipCountry(prevState => !prevState);
+  }
+
+  return (
+    <Modal opened={opened} onClose={close} title="Quick Play" centered>
+      <Stack>
+        <Box>What country is this?</Box>
+        {activeCountry && <Box><img src={`${(activeCountry as Country).flags.png}`} /></Box>}
+        <Box>{}</Box>
+        <TextInput
+          aria-label="Country name"
+          // description="Type /skip to skip"
+          placeholder="Country name" size="lg"
+          onChange={() => {
+            // console.log('value:', value)
+          }}/>
+        <SkipButton handleSkipCountry={handleSkipCountry} skipCountry={false} />
+      </Stack>
+    </Modal>
+  )
+}
+
+interface WelcomeModalProps {
+  handleSetIsQuickPlay: (value: boolean) => void;
+}
+
+function WelcomeModal(props: WelcomeModalProps): JSX.Element {
+  const [opened, { open, close }] = useDisclosure(true);
+
+  return (
+    <Modal opened={opened} onClose={close} title="Welcome to Banderas!" centered>
+      <Tabs style={{border: '1px solid tomato'}} orientation="vertical" defaultValue="welcome">
+        <Tabs.List>
+          <Tabs.Tab value="welcome">
+            Welcome
+          </Tabs.Tab>
+          <Tabs.Tab value="quick play">
+            Quick Play
+          </Tabs.Tab>
+          <Tabs.Tab value="rapid">
+            Rapid
+          </Tabs.Tab>
+          <Tabs.Tab value="explore">
+            Explore
+          </Tabs.Tab>
+        </Tabs.List>
+
+        <Tabs.Panel value="welcome">
+          Banderas is a game where you guess the country based on its flag.
+        </Tabs.Panel>
+
+        <Tabs.Panel value="quick play">
+          <Button onClick={() => {
+            props.handleSetIsQuickPlay(true);
+            close();
+            }}>
+            Quick Play
+          </Button>
+        </Tabs.Panel>
+
+        <Tabs.Panel value="rapid">
+          Rapid!
+        </Tabs.Panel>
+
+        <Tabs.Panel value="explore">
+          {/* <Button onClick={() => {
+            props.handleSetIsQuickPlay(true);
+            close();
+            }}> */}
+            Explore
+          {/* </Button> */}
+        </Tabs.Panel>
+          
+      </Tabs>
+    </Modal>
+  )
+}
+
+export default function Home(): JSX.Element {
 
   const [countries, setCountries] = useState<{}[]>([]);
+  const [isQuickPlay, setIsQuickPlay] = useState<boolean>(false);
+
+  const handleSetIsQuickPlay = () => {
+    setIsQuickPlay(true);
+  }
 
   useEffect(() => {
     if (countries.length === 0) {
@@ -24,13 +186,11 @@ export default function Home() {
 
   // window.scrollTo(0, document.body.scrollHeight);
   // window.scroll({top: document.body.scrollHeight, behavior: "smooth"});
-  const [opened, { open, close }] = useDisclosure(true);
 
   return (
     <>
-      <Modal className='julian' opened={opened} onClose={close} title="Welcome to Banderas!" centered>
-        <Button>Quick Play</Button>
-      </Modal>
+      <WelcomeModal handleSetIsQuickPlay={handleSetIsQuickPlay} />
+      {isQuickPlay && <QuickPlayModal countries={countries} />}
       <main className={styles.main}>
         <ul
           style={{
